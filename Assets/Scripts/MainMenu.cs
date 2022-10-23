e3e1ababe0326cb28691bb14bd44b60346c2e5a1
Daniel Liegl <d.liegl@student.tugraz.at>
2022-10-23 16:03:30 +0200

    main menu added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+  public void OnePlayerGame()
+  {
+    SceneManager.LoadScene(1);
+  }
+
+  public void TwoPlayerGame()
+  {
+    SceneManager.LoadScene(1);
+  }
+
+  public void QuitGame()
+  {
+    Application.Quit();
+  }
+
+
+}
